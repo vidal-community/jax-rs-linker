ac61baaca1d315751ae7eff7df21e9844ba3698a
Jean-Christophe GAY <contact@jeanchristophegay.com>
2014-12-29 17:21:22 +0100

    Migrate integration-tests to a webapp
    automatic tests not written yet.
    
    http://localhost:8080/it-tests/rest/product/1/self

---
+++ jax-rs-linker-integration-tests/src/main/java/net/biville/florent/jax_rs_linker/it/jersey/Configuration.java
@@ -0,0 +1,8 @@
+package net.biville.florent.jax_rs_linker.it.jersey;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("rest")
+public class Configuration extends Application {
+}
