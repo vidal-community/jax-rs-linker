7e7b5ecda76190385bf26cf8d832a35ed3675a44
@fbiville <florent.biville@gmail.com>
2015-01-26 14:08:03 +0100

    Issue #16. Test with web.xml webapp

---
+++ jax-rs-linker-integration-tests/xml-webapp/src/main/java/fr/vidal/oss/jax_rs_linker/it/jersey/Configuration.java
@@ -0,0 +1,10 @@
+package fr.vidal.oss.jax_rs_linker.it.jersey;
+
+import fr.vidal.oss.jax_rs_linker.api.ExposedApplication;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ExposedApplication(servletName = "fr.vidal.oss.jax_rs_linker.it.jersey.Configuration")
+public class Configuration extends Application {
+}
