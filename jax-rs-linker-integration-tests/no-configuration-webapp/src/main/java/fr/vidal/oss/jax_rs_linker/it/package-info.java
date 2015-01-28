755126d2855d6645df9831c5716f2085ca7dfe7a
@fbiville <florent.biville@gmail.com>
2015-01-28 10:22:07 +0100

    Import annotation and fix JAX-RS mention.

--- jax-rs-linker-integration-tests/no-configuration-webapp/src/main/java/fr/vidal/oss/jax_rs_linker/it/package-info.java
+++ jax-rs-linker-integration-tests/no-configuration-webapp/src/main/java/fr/vidal/oss/jax_rs_linker/it/package-info.java
@@ -1,2 +1,4 @@
-@fr.vidal.oss.jax_rs_linker.api.ExposedApplication(servletName = "javax.ws.rs.core.Application")
+@ExposedApplication(servletName = "javax.ws.rs.core.Application")
 package fr.vidal.oss.jax_rs_linker.it;
+
+import fr.vidal.oss.jax_rs_linker.api.ExposedApplication;
