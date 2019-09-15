742b22e9f0dfa4cde313d5de5b5ddc1fbb5579fb
Jean-Christophe Gay <contact@jeanchristophegay.com>
2019-09-15 11:58:00 +0200

    Separate api and processor implementation in two modules
    It allows to not leak the processors to transitive dependencies.
    The classes needed at runtime are separated in the 'api' module.
    
    With Maven you should now use:
    
                <dependency>
                    <groupId>fr.vidal.oss</groupId>
                    <artifactId>jax-rs-linker-api</artifactId>
                    <version>0.5-SNAPSHOT</version>
                </dependency>
                <dependency>
                    <groupId>fr.vidal.oss</groupId>
                    <artifactId>jax-rs-linker-processor</artifactId>
                    <version>O.5-SNAPSHOT</version>
                    <optional>true</optional>
                </dependency>
    
    to import jax-rs-linker.

---
+++ jax-rs-linker-api/src/test/java/fr/vidal/oss/jax_rs_linker/model/TemplatedUrlTest.java
@@ -0,0 +1,153 @@
+package fr.vidal.oss.jax_rs_linker.model;
+
+import fr.vidal.oss.jax_rs_linker.api.NoQueryParameters;
+import fr.vidal.oss.jax_rs_linker.api.PathParameters;
+import fr.vidal.oss.jax_rs_linker.api.QueryParameters;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.util.Collection;
+import java.util.regex.Pattern;
+
+import static fr.vidal.oss.jax_rs_linker.model.ProductQueryParameters.LABEL;
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TemplatedUrlTest {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void fails_to_render_value_with_unreplaced_parameters() {
+        thrown.expect(IllegalStateException.class);
+        thrown.expectMessage("Parameters to replace: id");
+
+        TemplatedUrl<ProductParameters, NoQueryParameters> templatedPath = templatedUrl(
+            "/product/{id}/brand",
+            singletonList(pathParameter(className("int"), "id")));
+
+        templatedPath.value();
+    }
+
+    @Test
+    public void renders_parameterless_path() {
+        TemplatedUrl<ProductParameters,NoQueryParameters> templatedUrl = templatedUrl(
+            "/product/",
+            emptyList());
+
+        assertThat(templatedUrl.value()).isEqualTo("/product/");
+    }
+
+    @Test
+    public void renders_parameterized_path_after_replacement() {
+        TemplatedUrl<ProductParameters,NoQueryParameters> templatedUrl = templatedUrl(
+            "/product/{id}",
+            singletonList(pathParameter(className("int"), "id")));
+
+        assertThat(
+            templatedUrl
+                .replace(ProductParameters.ID, "42")
+                .value()
+        ).isEqualTo("/product/42");
+    }
+
+    @Test
+    public void appends_a_single_query_parameter() {
+        TemplatedUrl<ProductParameters, ProductQueryParameters> templatedUrl = templatedUrl(
+            "/product/{id}",
+            singletonList(pathParameter(className("int"), "id")),
+            singletonList(queryParameter("label")));
+
+        assertThat(
+            templatedUrl
+                .replace(ProductParameters.ID, "42")
+                .append(LABEL, "bleue")
+                .value()
+        ).isEqualTo("/product/42?label=bleue");
+    }
+
+    @Test
+    public void appends_query_parameters_one_by_one() {
+        TemplatedUrl<ProductParameters, ProductQueryParameters> templatedUrl = templatedUrl(
+            "/product/{id}",
+            singletonList(pathParameter(className("int"), "id")),
+            singletonList(queryParameter("label")));
+
+        assertThat(
+            templatedUrl
+                .replace(ProductParameters.ID, "42")
+                .append(LABEL, "bleue")
+                .append(LABEL, "rouge")
+                .value()
+        ).isEqualTo("/product/42?label=bleue&label=rouge");
+    }
+
+    @Test
+    public void appends_query_parameters_at_once() {
+        TemplatedUrl<ProductParameters, ProductQueryParameters> templatedUrl = templatedUrl(
+            "/product/{id}",
+            singletonList(pathParameter(className("int"), "id")),
+            singletonList(queryParameter("label")));
+
+        assertThat(
+            templatedUrl
+                .replace(ProductParameters.ID, "42")
+                .appendAll(LABEL, asList("bleue", "rouge"))
+                .value()
+        ).isEqualTo("/product/42?label=bleue&label=rouge");
+    }
+
+    private TemplatedUrl<ProductParameters, NoQueryParameters> templatedUrl(String path, Collection<PathParameter> parameters) {
+        return new TemplatedUrl<>(path, parameters, emptyList());
+    }
+
+    private TemplatedUrl<ProductParameters, ProductQueryParameters> templatedUrl(String path, Collection<PathParameter> parameters, Collection<QueryParameter> queryParameters) {
+        return new TemplatedUrl<>(path, parameters, queryParameters);
+    }
+
+    private PathParameter pathParameter(ClassName className, String name) {
+        return new PathParameter(className, name);
+    }
+
+    private QueryParameter queryParameter(String name) {
+        return new QueryParameter(name);
+    }
+
+    private ClassName className(String type) {
+        return ClassName.valueOf(type);
+    }
+}
+
+enum ProductParameters implements PathParameters {
+    ID;
+
+    @Override
+    public String placeholder() {
+        return "id";
+    }
+
+    @Override
+    public Pattern regex() {
+        return null;
+    }
+}
+
+enum ProductQueryParameters implements QueryParameters {
+    LABEL("label");
+
+    private final String value;
+
+    ProductQueryParameters(String value) {
+
+        this.value = value;
+    }
+
+    @Override
+    public String value() {
+        return value;
+    }
+}
