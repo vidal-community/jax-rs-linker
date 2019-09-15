b4ffeecd5dd0fa1126e7153270a1106eab287d98
Jean-Christophe Gay <contact@jeanchristophegay.com>
2019-09-15 11:58:00 +0200

    Separate api and processor implementation in two modules
    It allows to not leak the processors to transitive dependencies.
    The classes needed at runtime are separated in the 'api' module.
    
    With Maven you should now use:
    
                <dependency>
                    <groupId>fr.vidal.oss</groupId>
                    <artifactId>jax-rs-linker-api</artifactId>
                    <version>0.5-SNAPSHOT</version>
                </dependency>
                <dependency>
                    <groupId>fr.vidal.oss</groupId>
                    <artifactId>jax-rs-linker-processor</artifactId>
                    <version>O.5-SNAPSHOT</version>
                    <optional>true</optional>
                </dependency>
    
    to import jax-rs-linker.

---
+++ jax-rs-linker-api/src/main/java/fr/vidal/oss/jax_rs_linker/functions/QueryParametersToQueryString.java
@@ -0,0 +1,27 @@
+package fr.vidal.oss.jax_rs_linker.functions;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.function.Function;
+
+public enum QueryParametersToQueryString implements Function<Map<String,Collection<String>>,String> {
+
+    TO_QUERY_STRING;
+
+    private static final String QUERY_OPERATOR = "?";
+
+    @Override
+    public String apply(Map<String, Collection<String>> input) {
+        if (input.isEmpty()) {
+            return "";
+        }
+        StringBuilder builder = new StringBuilder(QUERY_OPERATOR);
+        for (Map.Entry<String, Collection<String>> valuesPerKey : input.entrySet()) {
+            String key = valuesPerKey.getKey();
+            for (String value : valuesPerKey.getValue()) {
+                builder.append(key).append("=").append(value).append("&");
+            }
+        }
+        return builder.toString().substring(0, builder.length() - 1);
+    }
+}
---
+++ jax-rs-linker-api/src/test/java/fr/vidal/oss/jax_rs_linker/base/PreconditionsTest.java
@@ -0,0 +1,24 @@
+package fr.vidal.oss.jax_rs_linker.base;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class PreconditionsTest {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void do_not_throw_IllegalStateException_when_condition_is_true() {
+        Preconditions.checkState(true, "should not happen!");
+    }
+
+    @Test
+    public void throw_IllegalStateException_when_condition_is_false() {
+        thrown.expect(IllegalStateException.class);
+        thrown.expectMessage("must fail!");
+
+        Preconditions.checkState(false, "must fail!");
+    }
+}
