081c2808f924452db07db51daefef0cddb5c0d83
Jean-Christophe Gay <contact@jeanchristophegay.com>
2019-09-16 13:52:20 +0200

    Bump mockito from 2.0.31-beta to 3.0.0

--- jax-rs-linker-processor/src/test/java/fr/vidal/oss/jax_rs_linker/functions/MappingToDotTest.java
+++ jax-rs-linker-processor/src/test/java/fr/vidal/oss/jax_rs_linker/functions/MappingToDotTest.java
@@ -1,11 +1,20 @@
 package fr.vidal.oss.jax_rs_linker.functions;
 
 import com.google.common.collect.Lists;
-import fr.vidal.oss.jax_rs_linker.model.*;
+import fr.vidal.oss.jax_rs_linker.model.Api;
+import fr.vidal.oss.jax_rs_linker.model.ApiLink;
+import fr.vidal.oss.jax_rs_linker.model.ApiPath;
+import fr.vidal.oss.jax_rs_linker.model.ClassName;
+import fr.vidal.oss.jax_rs_linker.model.ClassNameGeneration;
+import fr.vidal.oss.jax_rs_linker.model.HttpVerb;
+import fr.vidal.oss.jax_rs_linker.model.JavaLocation;
+import fr.vidal.oss.jax_rs_linker.model.Mapping;
+import fr.vidal.oss.jax_rs_linker.model.SubResourceTarget;
+import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 
 import javax.lang.model.element.TypeElement;
 import java.util.Map;
@@ -15,8 +24,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
 public class MappingToDotTest {
+
+    @Rule
+    public MockitoRule mockito = MockitoJUnit.rule();
 
     @Mock
     Map.Entry<ClassNameGeneration, Mapping> entry;
@@ -41,7 +52,6 @@ public class MappingToDotTest {
     @Test
     public void maps_self_mapping_to_null() {
         ClassNameGeneration bar = new ClassNameGeneration(ClassName.valueOf("com.acme.Bar"), mock(TypeElement.class));
-        when(entry.getKey()).thenReturn(bar);
         when(entry.getValue()).thenReturn(new Mapping(
                 new JavaLocation(bar, "dealWithIt"),
                 new Api(
